c9737b2a156fd6857bc2abb9c200e791d1a6abf0
Ivan Lamparelli <lamparelli.ivan@gmail.com>
2020-04-13 12:25:48 +0200

    added printBoard logic

--- src/forza4.c
+++ src/forza4.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define rows 6
+#define cols 7
+int field[rows][cols];
+
+void printBoard();
+
 int main() {
 
+  printBoard();
   return 0;
+}
+
+void printBoard() {
+  for (int row = 0; row < rows; row++) {
+    for (int elem = 0; elem < cols; elem++) {
+        printf(" %d ", field[row][elem]);
+    }
+    printf("\n");
+  }
 }
