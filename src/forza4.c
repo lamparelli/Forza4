15ec856940ef9b79e17e0e2fb337a95621a1d711
Nico Bavaro <nico@MacBook-Pro-di-Nico.local>
2020-04-13 13:19:16 +0200

    get user input

--- src/forza4.c
+++ src/forza4.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define rows 6
 #define cols 7
 int field[rows][cols];
 
 void printBoard();
+void getMove(int coin);
+void setMove(int row, int col, int coin);
 
 int main() {
-
-  printBoard();
+  int coin = 1;
+  bool keepPlaying = true;
+  while(keepPlaying == true){
+    printBoard();
+    getMove(coin);
+  }
   return 0;
 }
 
@@ -20,4 +27,27 @@ void printBoard() {
     }
     printf("\n");
   }
+}
+
+void getMove(int coin){
+  int row, col;
+  //read row
+  do{
+    printf("Inserisci riga da 1 a %d: ", rows);
+    scanf("%d", &row);
+
+  }while(row < 1 || row > rows);
+  row--;
+  //read col
+  do{
+    printf("Inserisci colonna da 1 a %d: ", cols);
+    scanf("%d", &col);
+
+  }while(col < 1 || col > cols);
+  col--;
+  setMove(row, col, coin);
+}
+
+void setMove(int row, int col, int coin){
+  field[row][col] = coin;
 }
