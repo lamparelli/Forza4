c49297a0ac7ea400b406501b4b82d8acd0006e6b
Nico Bavaro <nico@MacBook-Pro-di-Nico.local>
2020-04-18 18:46:42 +0200

    init check victory condition

--- src/forza4.c
+++ src/forza4.c
@@ -12,14 +12,22 @@ void setMove(int col, int coin);
 bool isColFull(int col);
 bool isEmptyCell(int row, int col);
 int changePlayer(int coin);
+int checkVictory();
 
 int main() {
   int coin = 1;
+  int winner;
   bool keepPlaying = true;
   while(keepPlaying){
     printBoard();
     getMove(coin);
     coin = changePlayer(coin);
+    winner = checkVictory();
+    printf("\ndebug vincitore %d\n", winner);
+    if(winner != 0){
+      printf("\nVince il Giocatore %d\n", winner);
+      keepPlaying = false;
+    }
   }
   return 0;
 }
@@ -36,13 +44,17 @@ void printBoard() {
 
 void getMove(int coin){
   int col;
-  //read col
+  bool isValidMove;
+
+  //read user move
   do{
     printf("\n\nTocca a giocatore %d\n", coin);
     printf("Inserisci una colonna valida da 1 a %d: ", cols);
     scanf("%d", &col);
+    //converts col inserted by the user to its position in the array(cols go from 0 to n-1)
     col--;
-  }while(col < 0 || col >= cols || isColFull(col));
+    isValidMove = col >= 0 && col < cols && !isColFull(col);
+  }while(!isValidMove);
 
   setMove(col, coin);
 }
@@ -82,4 +94,24 @@ int changePlayer(int coin) {
   } else {
     return 1;
   }
+}
+
+// returns 0 if no player has won; else returns the winning player
+int checkVictory(){
+  int winner = 0;
+  const int victoryCondition = 4;
+  //checking victory conditions horizontally
+  for(int row = 0; row < rows || winner != 0; row++){
+    for(int cell = 0; cell <= cols - victoryCondition || winner != 0; cell++){
+      // if the player has put 4 in a row (if he has won)
+      printf("debug row %d, cell %d , valore %d, adiacente %d\n", row, cell, field[row][cell], field[row][cell+1]);
+      if(field[row][cell] == field[row][cell + 1] &&
+        field[row][cell] == field[row][cell + 2] &&
+        field[row][cell] == field[row][cell + 3]){
+          winner = field[row][cell];
+      }
+    }
+    printf("\n");
+  }
+  return winner;
 }
